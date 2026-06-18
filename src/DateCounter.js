b3487754075ab0ce7714cec75860f7fa43239ed0
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-06-18 04:06:46 +0330

    replacing useState whith useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,39 +1,41 @@
 import { useReducer, useState } from "react";
 
-function useReducer(state, action) {
-  console.log(state,action)
+function reducer(state, action) {
+  // console.log(state, action);
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
-
- const [count, dispatch] = useReducer(1) 
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    
-
-
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({ type: "inc" });
+    // dispatch(1);
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch(Number(e.target.value));
   };
 
   const reset = function () {
