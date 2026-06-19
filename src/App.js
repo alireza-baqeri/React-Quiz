7204a957f89f9726f33f63b39452029e7dcec74b
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-06-19 10:05:26 +0330

    adding the error handlers to the reducer and setting up the dispatch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import { type } from "@testing-library/user-event/dist/type";
 // import DateCounter from "./DateCounter";
 
-const intialState; = {}
+const initialState = {
+  questions: [],
+  // "loading", 'error','ready', 'active', 'finished'
+  status: "loading",
+};
+
 function reducer(state, action) {
-  
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: "error",
+      }
+    default:
+      throw new Error("Unknown Action");
+  }
 }
 
 export default function App() {
-  const [state,dispatch] = useReducer(reducer,intialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch(console.error("Error"))
-  }, []);
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }))}, []);
 
   return (
     <div className="app">
