6a0aa5d981362c0e4c5593f447687364a73cf35c
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-06-24 07:12:42 +0330

    The 3 Possible Statuses Are covered & added to the Main componnent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 // import { type } from "@testing-library/user-event/dist/type";
 // import DateCounter from "./DateCounter";
 
@@ -29,7 +32,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{quest,status}, dispatch] = useReducer(reducer, initialState);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -42,8 +45,13 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        
+        {status === 'loading' && <Loader />}
+        
+        {status === 'error' && <Error />}
+
+        {status === 'ready' && <StartScreen/>}
+        
       </Main>
       {/* <DateCounter/> */}
     </div>
---
+++ src/StartScreen.js
@@ -0,0 +1,17 @@
+function StartScreen() {
+    return (
+        <div>
+            <h2>
+                Welcome to The REACT Quiz!
+            </h2>
+            <h3>
+                X questions to test your React mastery
+            </h3>
+            <button>
+                Let's Begin
+            </button >
+        </div>
+    )
+}
+
+export default StartScreen
